ee3cee287471cd610a864f7190cfcbb35b830d7f
michaelpanciera <michael.panciera.work@gmail.com>
2015-10-28 19:05:34 -0400

    primes

--- ex4.hs
+++ ex4.hs
@@ -6,3 +6,11 @@ p x = 2*x + 1
 filter (not . lt) $  cartProd [1..n] [1..n]
 xs  = filter (not . ( >= n) . addij) $ filter (not . lt) $  cartProd [0..n] [0..n]
 map (\x -> (2*x)+ 1) $ concat [[a,b] | (a,b) <- xs]
+
+divides :: Int -> Int -> Bool
+divides x y = x `mod` y == 0
+isEmpty [] = True
+isEmpty _ = False
+range x = [0..x]
+isPrime :: Int -> Bool
+isPrime x = isEmpty $ filter odd $ filter (divides x)   [2..x-1]
